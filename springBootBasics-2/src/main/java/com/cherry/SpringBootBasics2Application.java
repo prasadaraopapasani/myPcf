4bf6ca9db18854f67248e348020e5653f4eefb00
prasad <papasani.java@gmail.com>
2018-06-02 15:31:28 +0530

    added project

---
+++ springBootBasics-2/src/main/java/com/cherry/SpringBootBasics2Application.java
@@ -0,0 +1,12 @@
+package com.cherry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasics2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasics2Application.class, args);
+	}
+}
